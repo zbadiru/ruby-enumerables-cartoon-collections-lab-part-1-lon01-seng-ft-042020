4ccc9451fd0cc77b7861711978bcdc8eef0ca669
Zakari Badiru <zbadiru@live.co.uk>
2020-04-04 20:17:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |array, index|
-    puts "#{index}.#{array}"
+    puts "#{index + 1}.#{array}"
 end
 end
